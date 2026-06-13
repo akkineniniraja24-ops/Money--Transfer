7a95aefbb2b43a08ec1e6ae472a784dd59fc8d2c
a.niraja <akkineniniraja24@gmail.com>
2026-06-13 15:52:13 +0530

    Update transaction.js

--- transaction.js
+++ transaction.js
@@ -1,13 +1,49 @@
-const mongoose = require("mongoose");
+async function loadHistory() {
 
-const transactionSchema = new mongoose.Schema({
-    sender: String,
-    receiver: String,
-    amount: Number,
-    date: {
-        type: Date,
-        default: Date.now
-    }
-});
+    const response = await fetch(
+        "http://localhost:5000/api/history"
+    );
 
-module.exports = mongoose.model("Transaction", transactionSchema);
+    const data = await response.json();
+
+    let html = "";
+
+    data.reverse().forEach(item => {
+
+        html += `
+        <tr>
+            <td>${item.sender}</td>
+            <td>${item.receiver}</td>
+            <td>₹${item.amount}</td>
+            <td>${new Date(item.date).toLocaleString()}</td>
+        </tr>
+        `;
+    });
+
+    document.getElementById("historyTable").innerHTML = html;
+}
+
+loadHistory();
+
+// Refresh every 3 seconds
+setInterval(loadHistory, 3000);
+async function sendMoney() {
+
+    const sender = document.getElementById("sender").value;
+    const receiver = document.getElementById("receiver").value;
+    const amount = document.getElementById("amount").value;
+
+    await fetch("http://localhost:5000/api/transaction", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            sender,
+            receiver,
+            amount
+        })
+    });
+
+    alert("Money Sent Successfully");
+}
